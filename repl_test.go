2f5ad310f3af88cb266ebb2a92c5d4c59e4227a4
cllong15 <17620963+cllong15@users.noreply.github.com>
2026-04-21 10:47:32 -0700

    CH1: REPL L3: Tests

--- repl_test.go
+++ repl_test.go
@@ -10,24 +10,34 @@ func TestCleanInput(t *testing.T) {
 		expected []string
 	}{
 		{
-			input:    "  Hello  WoRld  ",
+			input:    "  ",
+			expected: []string{},
+		},
+		{
+			input:    "  hello  ",
+			expected: []string{"hello"},
+		},
+		{
+			input:    "  hello  world  ",
 			expected: []string{"hello", "world"},
 		},
-		// add more cases here
+		{
+			input:    "  HellO  World  ",
+			expected: []string{"hello", "world"},
+		},
 	}
+
 	for _, c := range cases {
 		actual := cleanInput(c.input)
-		// Check the length of the actual slice against the expected slice
-		// if they don't match, use t.Errorf to print an error message
-		// and fail the test
+		if len(actual) != len(c.expected) {
+			t.Errorf("lengths don't match: '%v' vs '%v'", actual, c.expected)
+			continue
+		}
 		for i := range actual {
 			word := actual[i]
 			expectedWord := c.expected[i]
-			// Check each word in the slice
-			// if they don't match, use t.Errorf to print an error message
-			// and fail the test
 			if word != expectedWord {
-				t.Errorf("%s != %s", word, expectedWord)
+				t.Errorf("cleanInput(%v) == %v, expected %v", c.input, actual, c.expected)
 			}
 		}
 	}
