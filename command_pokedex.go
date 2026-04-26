f500d442349c88b9a49d9642fc5e21a130eb93d7
cllong15 <17620963+cllong15@users.noreply.github.com>
2026-04-26 14:26:24 -0700

    added pokedex command

--- command_pokedex.go
+++ command_pokedex.go
@@ -1,5 +1,11 @@
 package main
 
-// func commandPokedex(cfg *config, args ...string) error {
+import "fmt"
 
-// }
+func commandPokedex(cfg *config, args ...string) error {
+	fmt.Println("Your Pokedex:")
+	for _, poke := range cfg.caughtPokemon {
+		fmt.Printf(" - %s\n", poke.Name)
+	}
+	return nil
+}
